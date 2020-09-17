021f596fc62e92eeda12e9d0dd45355d7d6b6a5d
Ben Paige <ben.paige@rach.io>
2020-09-16 19:37:23 -0600

    first commit

---
+++ src/main/java/com/bgc/main/dao/PersonDao.java
@@ -0,0 +1,18 @@
+package com.bgc.main.dao;
+
+import com.bgc.main.model.Person;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    public List<Person> selectAllPeople();
+}
